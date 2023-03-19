405799157bccd26a191c14ff64cc5adf0926dd30
Marvel1411 <126253232+Marvel1411@users.noreply.github.com>
2023-03-19 14:19:39 +0000

    Will commit this bit of work, Shows start of basic input. Will continue it

--- box2d_scene/build/vs2017/Player.cpp
+++ box2d_scene/build/vs2017/Player.cpp
@@ -4,7 +4,27 @@
 
 void Player::update() {
 	//TODO: Handler input system here
+
+	if (keyboard_->IsKeyPressed(gef::Keyboard::KC_ESCAPE)) {
+		//pasues game
+	}
+	if (keyboard_->IsKeyPressed(gef::Keyboard::KC_SPACE)) {
+		//use power up, for when this is implemented
+	}
+
+
+	if (controller_->get_right_trigger() == true) {
+		float xaxis = controller_->left_stick_x_axis();
+		float yaxis = controller_->right_stick_y_axis();
+		//gets the position of the sticks and would fire one shotgun shot in said direction
+	}
+
+	if (controller_->buttons_pressed() & gef_SONY_CTRL_START) {
+		//pauses game
+	}
+
 	updatePhysics();
+
 }
 
 void Player::damage() {
--- box2d_scene/build/vs2017/Player.h
+++ box2d_scene/build/vs2017/Player.h
@@ -4,16 +4,28 @@
 
 #include <iostream>
 #include <Entity.h>
+#include <input/keyboard.h>
+#include <input/sony_controller_input_manager.h>
+#include <input/input_manager.h>
+#include <input/touch_input_manager.h>
 
 //#include <Windows.h>
-
+namespace gef {
+	class InputManager;
+}
 class Player : public Entity {
 public:
-	Player(PrimitiveBuilder& builder_, b2World& world_,
-		gef::Vector4* position_, gef::Quaternion* rotation_, gef::Vector4* scale_)
+	Player(PrimitiveBuilder& builder_, b2World& world_, gef::Vector4* position_, gef::Quaternion* rotation_, 
+		gef::Vector4* scale_)
+
 		: Entity(builder_, world_, position_, rotation_, scale_) {
 
 	}
+	
+	gef::InputManager* inputmanager_;
+	gef::Keyboard* keyboard_;
+	gef::SonyController* controller_;
+	gef::TouchInputManager* Mouse_;
 
 	virtual void update();
 	void damage();// { OutputDebugStringA("Player damaged!"); }
